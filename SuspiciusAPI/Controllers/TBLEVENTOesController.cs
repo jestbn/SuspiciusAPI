4a8310b9a5d721819f7e0d79f9587bd636092e2e
Juan Esteban Gil <juanestebang9@gmail.com>
2018-11-19 23:29:18 -0500

    Inicio> aplicacion implementada con bdCloud

---
+++ SuspiciusAPI/Controllers/TBLEVENTOesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AccesoDatos;
+
+namespace SuspiciusAPI.Controllers
+{
+    public class TBLEVENTOesController : ApiController
+    {
+        private SuspiciusDBEntities db = new SuspiciusDBEntities();
+
+        // GET: api/TBLEVENTOes
+        public IQueryable<TBLEVENTO> GetTBLEVENTO()
+        {
+            return db.TBLEVENTO;
+        }
+
+        // GET: api/TBLEVENTOes/5
+        [ResponseType(typeof(TBLEVENTO))]
+        public IHttpActionResult GetTBLEVENTO(int id)
+        {
+            TBLEVENTO tBLEVENTO = db.TBLEVENTO.Find(id);
+            if (tBLEVENTO == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tBLEVENTO);
+        }
+
+        // PUT: api/TBLEVENTOes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTBLEVENTO(int id, TBLEVENTO tBLEVENTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tBLEVENTO.id_evento)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tBLEVENTO).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TBLEVENTOExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/TBLEVENTOes
+        [ResponseType(typeof(TBLEVENTO))]
+        public IHttpActionResult PostTBLEVENTO(TBLEVENTO tBLEVENTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.TBLEVENTO.Add(tBLEVENTO);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = tBLEVENTO.id_evento }, tBLEVENTO);
+        }
+
+        // DELETE: api/TBLEVENTOes/5
+        [ResponseType(typeof(TBLEVENTO))]
+        public IHttpActionResult DeleteTBLEVENTO(int id)
+        {
+            TBLEVENTO tBLEVENTO = db.TBLEVENTO.Find(id);
+            if (tBLEVENTO == null)
+            {
+                return NotFound();
+            }
+
+            db.TBLEVENTO.Remove(tBLEVENTO);
+            db.SaveChanges();
+
+            return Ok(tBLEVENTO);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TBLEVENTOExists(int id)
+        {
+            return db.TBLEVENTO.Count(e => e.id_evento == id) > 0;
+        }
+    }
+}
---
+++ SuspiciusAPI/Controllers/TBLUSERsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AccesoDatos;
+
+namespace SuspiciusAPI.Controllers
+{
+    public class TBLUSERsController : ApiController
+    {
+        private SuspiciusDBEntities db = new SuspiciusDBEntities();
+
+        // GET: api/TBLUSERs
+        public IQueryable<TBLUSER> GetTBLUSER()
+        {
+            return db.TBLUSER;
+        }
+
+        // GET: api/TBLUSERs/5
+        [ResponseType(typeof(TBLUSER))]
+        public IHttpActionResult GetTBLUSER(int id)
+        {
+            TBLUSER tBLUSER = db.TBLUSER.Find(id);
+            if (tBLUSER == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tBLUSER);
+        }
+
+        // PUT: api/TBLUSERs/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTBLUSER(int id, TBLUSER tBLUSER)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tBLUSER.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tBLUSER).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TBLUSERExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/TBLUSERs
+        [ResponseType(typeof(TBLUSER))]
+        public IHttpActionResult PostTBLUSER(TBLUSER tBLUSER)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.TBLUSER.Add(tBLUSER);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = tBLUSER.ID }, tBLUSER);
+        }
+
+        // DELETE: api/TBLUSERs/5
+        [ResponseType(typeof(TBLUSER))]
+        public IHttpActionResult DeleteTBLUSER(int id)
+        {
+            TBLUSER tBLUSER = db.TBLUSER.Find(id);
+            if (tBLUSER == null)
+            {
+                return NotFound();
+            }
+
+            db.TBLUSER.Remove(tBLUSER);
+            db.SaveChanges();
+
+            return Ok(tBLUSER);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TBLUSERExists(int id)
+        {
+            return db.TBLUSER.Count(e => e.ID == id) > 0;
+        }
+    }
+}
