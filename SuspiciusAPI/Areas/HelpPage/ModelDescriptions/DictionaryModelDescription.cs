e3bc2852a96a3a616da4a4385cfe4b14f2d0be71
Juan Esteban Gil <juanestebang9@gmail.com>
2018-11-19 22:47:57 -0500

    Agregar archivos de proyecto.

---
+++ SuspiciusAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SuspiciusAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SuspiciusAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SuspiciusAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
